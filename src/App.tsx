26ae3a020eb5a1e605392be2818c7946ada383ed
Maximkalenkovich <maxim.peterson@mail.ru>
2023-11-26 17:58:48 +0300

    add useSate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const  [counter, setCounter] =    useState(0)
+
+  function increment() {
+      setCounter(counter + 1)
+  }
+
+  function decrement() {
+      setCounter(counter -1)
+  }
+
+
+    return (
+        <div className="App">
+          <h1> Счетчик : {counter}</h1>
+            <button onClick={increment}>Добавить</button>
+            <button onClick={decrement}>Убрать</button>
+        </div>
+    );
 }
+
+let a: string | null = 'dima';
+a = null;
+let name: string = 'it-kamasutra';
+let isSamurai: boolean | null = true;
+isSamurai = null;
+
+
+// let names : Array<string> = ['dimych','maxim', 'valera'];
+// let names2 : string[] = ['dimych','maxim', 'valera'];
+// // alert (names[0].toUpperCase())
+// names.forEach(n => {
+//   alert(n.toUpperCase())
+// })
+
+
+let sex: 'male' | 'femaly';
+
+
+type AddressType = {
+    city: string | null
+    country: string | null
+}
+
+
+let initialState = {
+    name: null as string | null,
+    age: null as number | null,
+    isSamurai: null as boolean | null,
+    addresses: [] as Array<AddressType>,
+    counter: 0
+}
+
+export type initialStateType = typeof initialState;
+
+let state: initialStateType = {
+    name: 'fdue',
+    age: 22,
+    addresses: [{city: 'dff', country: 'edfrf'}],
+    counter: 10,
+    isSamurai: true
+}
+
+
+
 
 export default App;
