9c83996512607b47ed0aca4332b5148b55dd4f98
Maximkalenkovich <maxim.peterson@mail.ru>
2023-11-26 18:54:07 +0300

    add useEffect and new useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,21 @@
-import React, {useState} from 'react';
-
-function computerInitialCounter() {
-    console.log('Some calculation...')
-    return Math.trunc(Math.random()*20)
-}
+import React, {useState, useEffect} from 'react';
 
 
 
 function App() {
-  const  [counter, setCounter] =    useState(() =>{
-      return computerInitialCounter()
-  })
+const [type, setType] =useState('users')
 
-  function increment() {
-     setCounter(prevState => {
-         return prevState + 1
-     })
-  // setCounter(prev => prev + 1)
-  }
-
-  function decrement() {
-      setCounter(counter -1)
-  }
-
-
+    useEffect(() => {
+         console.log('render')
+    }, []);
     return (
-        <div className="App">
-          <h1> Счетчик : {counter}</h1>
-            <button onClick={increment}>Добавить</button>
-            <button onClick={decrement}>Убрать</button>
+        <div >
+            <h1>Ресурс:{type}</h1>
+            <button onClick={ ()=>setType('users') }>Пользователи</button>
+            <button onClick={()=> setType('posts')}>Посты</button>
+            <button onClick={()=> setType('Todo')}>Todo</button>
         </div>
     );
-}
-
-let a: string | null = 'dima';
-a = null;
-let name: string = 'it-kamasutra';
-let isSamurai: boolean | null = true;
-isSamurai = null;
-
-
-// let names : Array<string> = ['dimych','maxim', 'valera'];
-// let names2 : string[] = ['dimych','maxim', 'valera'];
-// // alert (names[0].toUpperCase())
-// names.forEach(n => {
-//   alert(n.toUpperCase())
-// })
-
-
-let sex: 'male' | 'femaly';
-
-
-type AddressType = {
-    city: string | null
-    country: string | null
-}
-
-
-let initialState = {
-    name: null as string | null,
-    age: null as number | null,
-    isSamurai: null as boolean | null,
-    addresses: [] as Array<AddressType>,
-    counter: 0
-}
-
-export type initialStateType = typeof initialState;
-
-let state: initialStateType = {
-    name: 'fdue',
-    age: 22,
-    addresses: [{city: 'dff', country: 'edfrf'}],
-    counter: 10,
-    isSamurai: true
 }
 
 
---
+++ src/exempels/state.tsx
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+
+function computerInitialCounter() {
+    console.log('Some calculation...')
+    return Math.trunc(Math.random() * 20)
+}
+
+
+function Exempels() {
+    const [counter, setCounter] = useState(() => {
+        return computerInitialCounter()
+    })
+
+
+    function increment() {
+        setCounter(prevState => {
+            return prevState + 1
+        })
+        // setCounter(prev => prev + 1)
+    }
+
+    function decrement() {
+        setCounter(counter - 1)
+    }
+
+
+    const [state, setState] = useState({
+        title: 'Счетчик',
+        data: Date.now()
+    })
+
+
+    function updateTitle() {
+        setState(prev => {
+            return {
+                ...prev,
+                title: 'новое название'
+            }
+        })
+
+    }
+
+    return (
+        <div className="App">
+            <h1> Счетчик : {counter}</h1>
+            <button onClick={increment}>Добавить</button>
+            <button onClick={decrement}>Убрать</button>
+
+            <button onClick={updateTitle}>изменить название</button>
+            <pre>{JSON.stringify(state, null, 2)}</pre>
+        </div>
+    );
+}
+
+// let a: string | null = 'dima';
+// a = null;
+// let name: string = 'it-kamasutra';
+// let isSamurai: boolean | null = true;
+// isSamurai = null;
+
+
+// let names : Array<string> = ['dimych','maxim', 'valera'];
+// let names2 : string[] = ['dimych','maxim', 'valera'];
+// // alert (names[0].toUpperCase())
+// names.forEach(n => {
+//   alert(n.toUpperCase())
+// })
+
+
+// let sex: 'male' | 'femaly';
+//
+//
+// type AddressType = {
+//     city: string | null
+//     country: string | null
+// }
+//
+//
+// let initialState = {
+//     name: null as string | null,
+//     age: null as number | null,
+//     isSamurai: null as boolean | null,
+//     addresses: [] as Array<AddressType>,
+//     counter: 0
+// }
+//
+// export type initialStateType = typeof initialState;
+//
+// let state: initialStateType = {
+//     name: 'fdue',
+//     age: 22,
+//     addresses: [{city: 'dff', country: 'edfrf'}],
+//     counter: 10,
+//     isSamurai: true
+// }
+
+
+export default Exempels;
